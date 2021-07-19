d8e0ae8916a9916db362342fd8b5539610abf4a4
Ailes2 <fb.smirnov.u@gmail.com>
2021-07-19 20:42:55 +0300

    1

---
+++ src/com/company/Main.java
@@ -0,0 +1,46 @@
+package com.company;
+//
+public class Main {
+//        public static void main(String[] arg) {
+//            String str1 = "Fizz";
+//            String str2 = "Buzz";
+//
+//            for (int i = 1; i <= 100; i++) {
+//                if ((i % 3 == 0) && (i % 5 == 0))
+//                    System.out.println("i = "  + " " + str1 + str2);
+//                else if (i % 3 == 0)
+//                    System.out.println("i = "  + " " + str1);
+//                else if (i % 5 == 0)
+//                    System.out.println("i = "  + " " + str2);
+//                else System.out.println("i = " + i);
+//
+//
+//            }
+//        }
+//    }
+
+
+
+//public static void main(String[] args) {
+//    String str = "1234567890123456789";
+//    String reverse = new StringBuffer(str).reverse().toString();
+//      System.out.println("До реверса: " + str);
+//      System.out.println("После реверса: " + reverse);
+//}
+//}
+
+
+
+//    public static void main(String[] args) {
+//        System.out.println(isPalindrome("Лол"));
+//        System.out.println(isPalindrome("шабаш"));
+//        System.out.println(isPalindrome("Катака"));
+//        System.out.println(isPalindrome("Вася не валя"));
+//        System.out.println(isPalindrome("Лол о лол"));
+//        System.out.println(isPalindrome("  1  2  1  "));
+//
+//    }
+//    public static boolean isPalindrome(String str) {
+//        return str.equalsIgnoreCase(new StringBuffer(str).reverse().toString());
+//    }
+//}
